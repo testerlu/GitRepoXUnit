9cf3e0079314a3ff11b64934ae0344b551372eb5
testerlu <714267792@qq.com>
2019-11-26 10:13:16 +0800

    1

---
+++ src/test/java/TestJUnit4Demo.java
@@ -0,0 +1,16 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class TestJUnit4Demo {
+    @Test
+    public void testDemo(){
+        //System.out.println("hello");
+        assertTrue(true);
+    }
+
+    @Test
+    public void testDemo2(){
+        assertTrue(false);
+    }
+}
